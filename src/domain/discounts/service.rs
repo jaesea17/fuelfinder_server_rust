49cddf743bcb7d6f9a54997dea7ee804eddfd3e0
Joseph <jaesea@Josephs-MacBook-Air.local>
2026-03-18 22:36:05 +0100

    Update discount service behavior

--- src/domain/discounts/service.rs
+++ src/domain/discounts/service.rs
@@ -25,6 +25,7 @@ use crate::{
 pub struct DiscountService;
 
 const MAX_CODES_PER_IP_PER_STATION_PER_DAY: i64 = 1;
+const MAX_CODE_GENERATE_RETRY: i64 = 8;
 
 fn station_code_prefix(station_name: &str) -> String {
     let mut chars = station_name
@@ -178,7 +179,7 @@ impl DiscountService {
 
         let mut inserted: Option<DiscountCode> = None;
 
-        for _ in 0..8 {
+        for _ in 0..MAX_CODE_GENERATE_RETRY {
             let candidate_code = generate_candidate_code(&station_name, &station_type);
 
             let maybe_code = sqlx::query_as::<_, DiscountCode>(
